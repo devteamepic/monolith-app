00cfdcb1c3137e1b5bdc3895c91935af570c9921
paydaylight <maxat235@gmail.com>
2020-03-24 13:27:20 +0600

    seems working

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,14 +2,25 @@ import React from "react"
 import '../../assets/stylesheets/application.css';
 import Test from '../components/core/test'
 import { Route, Link } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import reducers from  '../redux/reducers'
+import { BrowserRouter } from 'react-router-dom'
+
+const createStoreMiddleware = applyMiddleware() (createStore);
 
 class App extends React.Component {
   render () {
+
     return (
-      <div className="App">
-        <Link style = {{ height: 'fit-content' }} to='/test'>Test</Link>
-        <Route path='/test' component={Test}/>
-      </div>
+        <Provider store={ createStoreMiddleware(reducers) }>
+            <BrowserRouter>
+                  <div className="App">
+                    <Link style = {{ height: 'fit-content' }} to='/test'>Test</Link>
+                    <Route path='/test' component={Test}/>
+                  </div>
+            </BrowserRouter>
+        </Provider>
     );
   }
 }
