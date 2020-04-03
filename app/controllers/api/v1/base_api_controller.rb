a0c55bc6acf9c25a8e82788c4e61d6d51e0a6a60
paydaylight <maxat235@gmail.com>
2020-04-03 13:51:57 +0600

    cors issues fixed

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::BaseApiController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :doorkeeper_authorize!
+  before_action :allow_all
 
+  def allow_all
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
+  
   def authenticate_user
     unless current_user
       render json: {
