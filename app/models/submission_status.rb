6e42188d69511c571990c81e7552f9c9101b2297
paydaylight <maxat235@gmail.com>
2020-05-22 19:03:16 +0600

    fixed to proto methods

--- app/models/submission_status.rb
+++ app/models/submission_status.rb
@@ -1,2 +1,6 @@
 class SubmissionStatus < ApplicationRecord
+  def to_proto
+    # RailsApi::DocumentsSubmission::Status.encode(RailsApi::DocumentsSubmission::Status.new(id: id, name: name))
+    RailsApi::DocumentsSubmission::Status.new(id: id, name: name)
+  end
 end
